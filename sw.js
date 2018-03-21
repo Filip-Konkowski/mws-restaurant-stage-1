d6928c09114a08f8aa09c26d4e66b0a8cbbe73d5
filip <fkonkowski@viprsolutions.com>
2018-03-21 18:47:50 +0000

    SW with caching

--- sw.js
+++ sw.js
@@ -1,19 +1,37 @@
+var staticCacheName = 'mws-v1';
+
 self.addEventListener('install', function(event) {
+
     event.waitUntil(
-        caches.open('mws-v1').then(function(cache){
-            return cache.addAll(
-                [
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll([
                     '/',
-                    '/js/dbhelper.js',
-                    '/js/main.js',
-                    '/js/restaurant_info.js',
-                    '/css/styles.css',
-                    '/css/styles-medium.css',
-                    '/css/styles-large.css'
-                ]
-            )
+                    'js/dbhelper.js',
+                    'js/main.js',
+                    'js/restaurant_info.js',
+                    'css/styles.css',
+                    'css/styles-medium.css',
+                    'css/styles-large.css',
+                ]);
         })
-    )
+    );
+});
+
+
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('mws-') &&
+                        cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
