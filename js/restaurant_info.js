ce59e7647b447ce69f00c3dfd6b3a528f730dc33
filip <fkonkowski@viprsolutions.com>
2018-03-25 17:18:34 +0100

    Add aria

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -131,6 +131,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
         return;
     }
     const ul = document.getElementById('reviews-list');
+    ul.setAttribute("aria-labelledby", title.innerHTML);
     reviews.forEach(review => {
         ul.appendChild(createReviewHTML(review));
     });
