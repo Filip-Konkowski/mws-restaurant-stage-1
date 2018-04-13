df4a02361bc8901131b4ed4df902631c8a820a06
filip <fkonkowski@viprsolutions.com>
2018-04-13 17:51:08 +0100

    Remove not needed indexed DB

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -24,13 +24,6 @@ class DBHelper {
         .then(response => response.json())
         .then(data => {
             const restaurants = data;
-            console.log(restaurants)
-
-            idb = new IndexedDBHelper();
-            console.log('idb.idb',idb.idb)
-            objectStore = idb.open('mws', 1);
-
-            console.log('objectStore',objectStore)
             callback(null, restaurants);
         })
         .catch(function(e) {
@@ -39,11 +32,6 @@ class DBHelper {
 
   }
     static requestError(e) {
-        idb = new IndexedDBHelper();
-        objectStore = idb.open('mws', 1);
-        console.log('custom IDB', idb)
-        console.log('objectstore', objectStore);
-
         console.log('fetch error: ', e);
   }
   /**
