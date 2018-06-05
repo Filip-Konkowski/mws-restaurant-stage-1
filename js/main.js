31c48d8427111d3dc2f0e0157b0ca58cfcafa1dd
filip <fkonkowski@viprsolutions.com>
2018-06-05 21:07:19 +0100

    Css/HTML button

--- js/main.js
+++ js/main.js
@@ -178,10 +178,20 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+    const favorite = document.createElement('span');
+    const favoriteLink = document.createElement('a');
+    favoriteLink.innerHTML = 'My favorite';
+    favoriteLink.className = 'favorite-button';
+    favoriteLink.id = restaurant.id;
+    favorite.append(favoriteLink);
+    li.append(favorite)
+
+
+    const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
+
 
   return li
 }
